096f0665f7506c75bff2636641d2ea1d4bb2731c
KurbanovMurad <kurbanov.m.84@mail.ru>
2021-02-03 19:32:17 +0300

    Site Subheader

--- src/pages/Components/Components.tsx
+++ src/pages/Components/Components.tsx
@@ -1,19 +1,20 @@
 import React, { FC } from 'react';
 import Button from '../../components/Button/Button';
 import Header from '../../components/Header/Header';
-import { COLORS } from '../../constants';
+import SubHeader from '../../components/SubHeader/SubHeader';
 
 interface IExternalProps {}
 
 interface IProps extends IExternalProps {}
 
-const Components:FC<IProps> = () => {
+const Components: FC<IProps> = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <SubHeader />
       <Button>ОСТАВИТЬ ОТЗЫВ</Button>
     </div>
   );
-}
+};
 
 export default Components;
