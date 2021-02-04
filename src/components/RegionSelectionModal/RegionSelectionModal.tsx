955eb03e934f5539620330993d1bada77e27418f
KurbanovMurad <kurbanov.m.84@mail.ru>
2021-02-04 21:13:47 +0300

    save

---
+++ src/components/RegionSelectionModal/RegionSelectionModal.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import './RegionSelectionModal.scss';
+
+interface IExternalProps {}
+
+interface IProps extends IExternalProps {}
+
+
+const RegionSelectionModal: FC<IProps> = () => {
+  return (
+    <div>
+      <button className="myBtn"> Open Modal </button>
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close"> &times </span>
+          <p>Модальное окно</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegionSelectionModal;
