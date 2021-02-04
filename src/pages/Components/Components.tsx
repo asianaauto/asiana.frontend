f9c139c09d25a7201f7c64485e4273ede99c80eb
KurbanovMurad <kurbanov.m.84@mail.ru>
2021-02-04 10:51:36 +0300

    floating button

--- src/pages/Components/Components.tsx
+++ src/pages/Components/Components.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import Button from '../../components/Button/Button';
 import Header from '../../components/Header/Header';
 import SubHeader from '../../components/SubHeader/SubHeader';
+import FloatingButton from '../../components/FloatingButton/FloatingButton';
 
 interface IExternalProps {}
 
@@ -13,6 +14,7 @@ const Components: FC<IProps> = () => {
       <Header />
       <SubHeader />
       <Button>ОСТАВИТЬ ОТЗЫВ</Button>
+      <FloatingButton />
     </div>
   );
 };
